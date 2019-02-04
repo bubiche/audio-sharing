e7565c6a33bc4ec8c3b9874ca56bbd48d73954f8
Nguyen Nguyen <npnguyen@apcs.vn>
2019-02-04 22:22:32 +0700

    add `queued_storage`'s settings

--- commons/settings/prod.py
+++ commons/settings/prod.py
@@ -8,10 +8,14 @@ from queued_storage.backends import QueuedStorage
 DEBUG = False
 ALLOWED_HOSTS = ['chuc-be-ngu-ngon.herokuapp.com']
 
+
+# Dropbox storage settings
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = os.environ.get('DROPBOX_OAUTH2_TOKEN', '')
 DROPBOX_ROOT_PATH = '/dropbox_api_storage'
 
+
+# Caches
 CACHES = {
     'default': {
         'BACKEND': 'django_bmemcached.memcached.BMemcached',
@@ -23,4 +27,8 @@ CACHES = {
     }
 }
 
+
+# Queued storage settings
 INSTALLED_APPS = INSTALLED_APPS + ['queued_storage']
+QUEUED_STORAGE_RETRIES = 2
+QUEUED_STORAGE_RETRY_DELAY = 60
