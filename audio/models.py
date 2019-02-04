001d8030cd2f58e81a0829633251513495aa9164
Nguyen Nguyen <npnguyen@apcs.vn>
2019-02-04 22:49:58 +0700

    band-aid to make `queued_storage` work with new django version

--- audio/models.py
+++ audio/models.py
@@ -3,18 +3,67 @@ from django.core.validators import URLValidator
 from django.conf import settings
 
 import uuid
+import types
 from queued_storage.backends import QueuedStorage
 
 from commons.custom_model_fields import VariableStorageFileField
+
+
+def get_accessed_time(self, name):
+    """
+    Django +1.10
+    Returns the last accessed time (as datetime object) of the file
+    specified by name.
+     :param name: file name
+    :type name: str
+    :rtype: :class:`~python:datetime.datetime`
+    """
+    return self.get_storage(name).accessed_time(name)
+
+
+def get_created_time(self, name):
+    """
+    Django +1.10
+    Returns the creation time (as datetime object) of the file
+    specified by name.
+     :param name: file name
+    :type name: str
+    :rtype: :class:`~python:datetime.datetime`
+    """
+
+    return self.get_storage(name).created_time(name)
+
+
+def get_modified_time(self, name):
+    """
+    Django +1.10
+    Returns the last modified time (as datetime object) of the file
+    specified by name.
+     :param name: file name
+    :type name: str
+    :rtype: :class:`~python:datetime.datetime`
+    """
+
+    return self.get_storage(name).modified_time(name)
+
+
+def generate_filename(self, filename):
+    return self.get_storage(filename).generate_filename(filename)
 
 
 def get_default_storage():
     if settings.DEBUG:
         return None
 
-    return QueuedStorage(
+    storage = QueuedStorage(
         'django.core.files.storage.FileSystemStorage',
         'storages.backends.dropbox.DropBoxStorage', task='queued_storage.tasks.TransferAndDelete')
+
+    storage.get_accessed_time = types.MethodType(get_accessed_time, storage, QueuedStorage)
+    storage.get_created_time = types.MethodType(get_created_time, storage, QueuedStorage)
+    storage.get_modified_time = types.MethodType(get_modified_time, storage, QueuedStorage)
+    storage.generate_filename = types.MethodType(generate_filename, storage, QueuedStorage)
+    return storage
 
 
 class AudioUpload(models.Model):
