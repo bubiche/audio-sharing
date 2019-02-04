4b07b6bc0069605f3d1a6d54e39a14e2c5a24191
Nguyen Nguyen <npnguyen@apcs.vn>
2019-02-04 21:32:58 +0700

    attempt to fix celery on heroku :(

--- commons/celery.py
+++ commons/celery.py
@@ -1,8 +1,11 @@
 from celery import Celery
 from django.conf import settings
+import os
 
 
-app = Celery('root')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commons.settings')
+
+app = Celery('commons')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
